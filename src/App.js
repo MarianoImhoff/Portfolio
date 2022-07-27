c00610b91cf01db13616773762e60b7d90ff1785
imhmariano <imhmariano@gmail.com>
2022-07-27 15:16:52 -0300

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Router, Routes } from 'react-router'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+      <Routes>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return (
+      <div>
+        Mariano Imhoff
+      </div>
+    );
+  }
+  
+  export default Navbar;
